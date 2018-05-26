d0729a1f26b0f0c2f56f2b95d5c017a2019aff78
sergey karabuta <siriusitsastar@gmail.com>
2018-05-26 13:01:47 +0300

    second version

--- ClassWork26_05_18/Program.cs
+++ ClassWork26_05_18/Program.cs
@@ -44,11 +44,83 @@ namespace ClassWork26_05_18
             //Console.WriteLine("Hello");
             //BreakOut: Console.WriteLine("BreakeOut");
 
-            int[] arrayOfInt = { 1, 2, 10, 23 };
+            //int[] arrayOfInt = { 1, 2, 10, 23 };
 
-            foreach(int i in arrayOfInt)
+            //foreach(int i in arrayOfInt)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //somthing about IF statement
+            //int a = 1;
+            //if (a > 0)
+            //{
+            //    Console.WriteLine("a>0");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("a<=0");
+            //}
+
+            ////or
+
+            //string s = a > 0 ? "a>0":"a<=0";
+            //Console.WriteLine(s);
+
+            //something about bits
+
+            //for(int i=0; i<8;i++)
+            //{
+            //    int result = 1 << i;
+            //    Console.WriteLine(result);
+            //}
+
+            //5.1 exersise
+            //for(int i=0;i<100;i++)
+            //{
+            //    Console.Write("a");
+            //}
+            //for(int i=0;i<100;i++)
+            //{
+            //    Console.WriteLine("a");
+            //}
+
+            //5.2 exersise
+            //for(int i=0;i<=255;i++)
+            //{
+            //    Console.Write("{0} ", i);
+            //}
+            //Console.WriteLine();
+            //for(int i=255;i>=0;i--)
+            //{
+            //    Console.Write("{0} ", i);
+            //}
+
+            //5.3 exersise
+            int a, b;
+            int result;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            if(a>b)
             {
-                Console.WriteLine(i);
+                result = a - b;
+            }
+            else
+            {
+                result = b - a;
+            }
+
+            //Or
+            int maxValue, minValue;
+            maxValue = a > b ? a : b;
+            minValue = a < b ? a : b;
+
+            for(int i=result;i>0;i--)
+            {
+                if(i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
             }
 
 
