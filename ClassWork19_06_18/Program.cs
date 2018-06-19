f0d938a900df55edf430b1a0e4f74d318d3467c7
visualstudioitcloud55 <visualstudioitcloud55@outlook.com>
2018-06-19 20:47:24 +0300

    changes from class

---
+++ ClassWork19_06_18/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ItCloud.Education.MathLibs;
+
+
+
+namespace ClassWork19_06_18
+{
+    public struct Product
+    {
+        int ID;
+        string NAME;
+        decimal PRICE;
+
+        public Product(int id,string name, decimal price)
+        {
+            ID = id;
+            NAME = name;
+            PRICE = price;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Book id: {ID}, Name = {NAME}, Price = {PRICE}");
+        }
+
+    }
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            int val = 0;
+            int result = Incrementor.Inc(ref val);
+
+            int val1;
+            string str;
+            Incrementor.Intout(out val1,out str);
+            Console.WriteLine(val1);
+            Console.WriteLine(str);
+
+            Product[] arrayOfProducts;
+            Console.WriteLine("Enter number of products");
+            int numberOfProducts = int.Parse(Console.ReadLine());
+            arrayOfProducts = new Product[numberOfProducts];
+
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+
+            }
+            
+        }
+    }
+}
---
+++ ItCloud.Education.MathLibs/Incrementor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItCloud.Education.MathLibs
+{
+    public static class Incrementor
+    {
+        public static int Inc(ref int val)
+        {
+            ++val;
+            return 1;
+        }
+
+        public static int Intout(out int val1, out string str)
+        {
+            val1 = 1;
+            str = "something";
+            return 0;
+        }
+    }  
+
+}
