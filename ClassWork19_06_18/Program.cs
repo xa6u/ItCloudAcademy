d831478aa947796112d4c0e783f2561a5e1d505d
s.karabuta@langate.com <s.karabuta@langate.com>
2018-06-21 16:38:13 +0300

    qwewrqwe

--- ClassWork19_06_18/Program.cs
+++ ClassWork19_06_18/Program.cs
@@ -31,26 +31,52 @@ namespace ClassWork19_06_18
 
     class Program
     {
+
+        public static void Prodmaker(int numberOfProducts)
+        {
+           // int i = 0;
+            int id;
+            string name;
+            decimal price;
+            Console.WriteLine($"Enter {numberOfProducts} product id");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter {numberOfProducts} product name");
+            name = Console.ReadLine();
+            Console.WriteLine($"Enter {numberOfProducts} product price");
+            price = int.Parse(Console.ReadLine());
+        }
+
+        public static void Sortbyprice(params object[] arrayOfProducts)
+        {
+            for (int i = 0; i < arrayOfProducts.Length; i++)
+            {
+                if (arrayOfProducts.[i] > arrayOfProducts[i+1])
+
+                
+
+            }
+        }
         
         static void Main(string[] args)
         {
 
-            int val = 0;
-            int result = Incrementor.Inc(ref val);
+            //int val = 0;
+            //int result = Incrementor.Inc(ref val);
 
-            int val1;
-            string str;
-            Incrementor.Intout(out val1,out str);
-            Console.WriteLine(val1);
-            Console.WriteLine(str);
+            //int val1;
+            //string str;
+            //Incrementor.Intout(out val1,out str);
+            //Console.WriteLine(val1);
+            //Console.WriteLine(str);
 
             Product[] arrayOfProducts;
             Console.WriteLine("Enter number of products");
             int numberOfProducts = int.Parse(Console.ReadLine());
             arrayOfProducts = new Product[numberOfProducts];
 
-            for (int i = 0; i < numberOfProducts; i++)
+            for (int i = 1; i <= numberOfProducts; i++)
             {
+                Prodmaker(i);
 
             }
             
