a47b7b8f56df5f0794442db61324c06106fdb83b
s.karabuta@langate.com <s.karabuta@langate.com>
2018-06-26 14:48:58 +0300

    homework

---
+++ Homework 22.06.18/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_22._06._18
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstElement;
+            int secondElement;
+            int summ;
+            string s1, s2;
+
+            Random random = new Random();
+            int[] intArray = new int[10];
+
+            for (int i = 1; i < 10; i++)
+            {
+                int randomArrayValue = random.Next(1000);
+                intArray[i] = randomArrayValue;
+                Console.WriteLine(intArray[i]);
+            }
+
+            Console.WriteLine("Enter index of first element");
+            s1 = Console.ReadLine();
+            firstElement = int.Parse(s1);
+            Console.WriteLine("Enter index of second element");
+            s2 = Console.ReadLine();
+            secondElement = int.Parse(s2);
+
+
+
+            try
+            {
+                if (firstElement > intArray.Length || secondElement > intArray.Length || firstElement < intArray.Length || secondElement < intArray.Length)
+                {
+                    throw new IndexOutOfRangeException("The element is not in the array");
+                }
+
+                summ = intArray[firstElement] + intArray[secondElement];
+
+                Console.WriteLine(summ);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
