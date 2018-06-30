639d060fd5e21c890feb6b8ae5d90161605ddfa8
visualstudioitcloud55 <visualstudioitcloud55@outlook.com>
2018-06-30 13:03:02 +0300

    changes from classwork

---
+++ ClassWorkk 30.06.18/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWorkk_30._06._18
+{
+
+    class ArrayRandom
+    {
+        private int[] _array;
+
+        public int MaxElementValue
+        {
+            get
+            {
+                int maxVal = _array.Max();
+                return maxVal;
+
+            }
+        }
+
+        public int MinElementValue
+        {
+            get
+            {
+                int minVal = _array.Min();
+                return minVal;
+            }
+        }                 
+        
+        public ArrayRandom()
+        {
+            var randome = new Random();
+
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                array[i] = randome.Next(10, 100);
+            }
+            _array = array;
+        }
+
+        public void DisplayArray()
+        {
+            foreach (var item in _array)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    
+    }
+
+    class Calculator
+    {
+        private int _someInt;
+    
+
+        public Calculator(int value)
+         {
+            _someInt = value;
+         }
+        
+        
+        public int Add(int addInt)
+        {
+            int summ;
+            summ = _someInt + addInt;
+            return summ;
+        }
+
+        public int Inc(int incInt)
+        {
+            int inc;
+            inc = ++incInt ;
+            return inc;
+
+        }
+    }
+
+    class HumenClass
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public HumenClass(string name)
+        {
+            Name = name;
+        }
+    }
+
+    struct HumenStruct
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public HumenStruct(string name)
+        {
+            Name = name;
+            Age = -1;
+        }
+    }
+    
+    class IntegerContainer
+    {
+        private int[] _array = { 12, 143, 54, 76, 2, 4, 6, 8, 90, 67 };
+
+        public decimal MIddleArythmetic
+        {
+            get
+            {
+                int totalSum = 0;
+                foreach (int item in _array)
+                {
+                    totalSum += item;
+                }
+                return (decimal)totalSum / _array.Length;
+            }
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var a = new IntegerContainer();
+
+            //Console.WriteLine($"A = {a.MIddleArythmetic}");
+
+            //HumenClass humenClass = new HumenClass("Victor");
+            //HumenStruct humenStruct = new HumenStruct();
+            //Console.WriteLine(humenClass);
+            //Console.WriteLine(humenStruct);
+            //Console.WriteLine();
+
+            //Calculator calc = new Calculator(25);
+            //Console.WriteLine(calc.Add(10));
+            //Console.WriteLine(calc.Inc(10));
+
+
+
+
+        }
+    }
+}
---
+++ HomeWork30.06.18/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork30._06._18
+{
+    class Book
+    {
+        private string _author;
+        private string _bookName;
+        private int _numberOfPages;
+
+        public Book()
+        {
+            _author = "unknown";
+            _bookName = "unknown";
+            _numberOfPages = 0;
+        }
+
+        public Book(string author, string bookName)
+        {
+            _author = author;
+            _bookName = bookName;
+        }
+
+        public Book(string author, string bookName, int numberOfPages)
+        {
+            _author = author;
+            _bookName = bookName;
+            _numberOfPages = numberOfPages;
+
+        }
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
