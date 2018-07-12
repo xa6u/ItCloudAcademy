da223ef123870eb70a73688fcad44e326927aca6
visualstudioitcloud55 <visualstudioitcloud55@outlook.com>
2018-07-12 20:43:08 +0300

    classwork

---
+++ ClassWork07.12.18/Car.cs
@@ -0,0 +1,37 @@
+﻿using ClassWork07._12._18.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork07._12._18
+{
+    class Car : IPrintLog, IFuel, IMoving
+    {
+        private int _tankState;
+
+        public FuelType Type => throw new NotImplementedException();
+
+        public int TankVolume { get; private set; }
+
+        public int TankState => _tankState;
+
+
+        public void FillTank(int newFuel)
+        {
+            throw new NotImplementedException();
+        }
+
+        //private int 
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintInfo(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
